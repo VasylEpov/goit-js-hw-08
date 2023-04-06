de598a5f6ec0744237b75a40e823aa1d5a35895c
VasylEpov <vepov86@gmail.com>
2023-04-06 03:48:10 +0300

    hmJs8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,46 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+//dom links
+const galleryContainer = document.querySelector('.gallery');
+
+const markupGallery = createGaleryMarkup(galleryItems);
+
+//events
+
+galleryContainer.insertAdjacentHTML('beforeend', markupGallery);
+
+
+//functions
+
+function createGaleryMarkup(galleryItems) {
+   return  galleryItems.map(({ preview, original, description }) => {
+       return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" 
+                src="${preview}" 
+                alt="${description}" 
+                />
+            </a>
+        </li> 
+    `;
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition : "bottom",
+  captionDelay: 250,
+  closeOnEscape: true
+})
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const FORM_STORAGE_KEY = 'feedback-form-state';
+
+const formState = JSON.parse(localStorage.getItem(FORM_STORAGE_KEY)) || {};
+
+const formInputs = {
+  form: document.querySelector('.feedback-form'),
+  messageInput: document.querySelector('textarea'),
+  emailInput: document.querySelector('input'),
+};
+
+formInputs.form.addEventListener('submit', onFormSubmit);
+formInputs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function updateFormData() {
+  formState.email = formInputs.emailInput.value;
+  formState.message = formInputs.messageInput.value;
+
+  localStorage.setItem(FORM_STORAGE_KEY, JSON.stringify(formState));
+
+  if (formState.email) {
+    formInputs.emailInput.value = formState.email;
+  }
+
+  if (formState.message) {
+    formInputs.messageInput.value = formState.message;
+  }
+}
+
+function onFormInput() {
+  updateFormData();
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  if (!formState.email || !formState.message) {
+    alert(`Будь ласка, заповніть всі обов'язкові поля.`);
+    return;
+  }
+
+  localStorage.removeItem(FORM_STORAGE_KEY);
+  evt.currentTarget.reset();
+  formState.email = '';
+  formState.message = '';
+}
